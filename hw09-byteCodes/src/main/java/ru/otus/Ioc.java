a11c41bcfd14db39b9f3cdf8c78be079b84eeeea
mikadzes <mikadzes@gmail.com>
2020-06-01 23:32:27 +0300

    hw09 fix

--- hw09-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw09-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -5,6 +5,9 @@ import ru.otus.annotations.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class Ioc {
@@ -20,17 +23,21 @@ class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final MyClassInterface myClass;
+        private List<String> methods;
 
         DemoInvocationHandler(MyClassInterface myClass) {
             this.myClass = myClass;
+            this.methods = Arrays.stream(myClass.getClass().getDeclaredMethods())
+                    .filter((method) -> method.isAnnotationPresent(Log.class))
+                    .map(Method::getName)
+                    .collect(Collectors.toList());
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Method methodImpl = myClass.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
             System.out.println();
-            if (methodImpl.isAnnotationPresent(Log.class)) {
-                System.out.println("executed method: " + methodImpl.getName() + ", param: ");
+            if (methods.contains(method.getName())) {
+                System.out.println("executed method: " + method.getName() + ", param: ");
                 Stream.of(args).forEach(System.out::println);
             }
             return method.invoke(myClass, args);
