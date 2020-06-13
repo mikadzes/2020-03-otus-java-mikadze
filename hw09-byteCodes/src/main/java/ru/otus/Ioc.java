48a2c8b00844392469ba1d9aca38271bafa5406b
mikadzes <mikadzes@gmail.com>
2020-06-13 15:02:16 +0300

    hw09 fix

--- hw09-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw09-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -6,7 +6,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,14 +23,14 @@ class Ioc {
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final MyClassInterface myClass;
-        private List<String> methods;
+        private Set<String> methods;
 
         DemoInvocationHandler(MyClassInterface myClass) {
             this.myClass = myClass;
             this.methods = Arrays.stream(myClass.getClass().getDeclaredMethods())
                     .filter((method) -> method.isAnnotationPresent(Log.class))
                     .map(Method::getName)
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toSet());
         }
 
         @Override
@@ -38,7 +38,7 @@ class Ioc {
             System.out.println();
             if (methods.contains(method.getName())) {
                 System.out.println("executed method: " + method.getName() + ", param: ");
-                Stream.of(args).forEach(System.out::println);
+                Stream.of(args != null ? args : new Object[0]).forEach(System.out::println);
             }
             return method.invoke(myClass, args);
         }
--- hw09-byteCodes/src/main/java/ru/otus/ProxyDemo.java
+++ hw09-byteCodes/src/main/java/ru/otus/ProxyDemo.java
@@ -3,7 +3,9 @@ package ru.otus;
 public class ProxyDemo {
     public static void main(String[] args) {
         MyClassInterface myClass = Ioc.createMyClass();
+        myClass.secureAccess();
         myClass.secureAccess("Security Param");
+        myClass.secureAccess("Security Param","overloading");
         myClass.methodWithoutAnnotation(1);
         myClass.methodWithAnnotationAndTwoParameters(2, 3);
     }
